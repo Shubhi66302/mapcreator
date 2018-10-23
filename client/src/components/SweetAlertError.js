a6338bac8f8dd9d6707e04f7fc77ed3af3f595cc
Arpit Tarang Saxena <arpit.s@greyorange.sg>
2018-10-23 12:49:32 +0530

    parse json error correctly in sweetalert

--- client/src/components/SweetAlertError.js
+++ client/src/components/SweetAlertError.js
@@ -1,12 +1,18 @@
 import React from "react";
 import SweetAlert from "react-bootstrap-sweetalert";
 
-export default ({ error, onConfirm, title = "Error" }) => (
-  <SweetAlert show={error != null} error title={title} onConfirm={onConfirm}>
-    <pre>
-      <code>
-        {error == null ? "" : `${JSON.stringify(JSON.parse(error), null, 2)}`}
-      </code>
-    </pre>
-  </SweetAlert>
-);
+export default ({ error, onConfirm, title = "Error" }) => {
+  let parsedError;
+  try {
+    parsedError = JSON.stringify(JSON.parse(error), null, 2);
+  } catch (e) {
+    parsedError = error;
+  }
+  return (
+    <SweetAlert show={error != null} error title={title} onConfirm={onConfirm}>
+      <pre>
+        <code>{error == null ? "" : `${parsedError}`}</code>
+      </pre>
+    </SweetAlert>
+  );
+};
