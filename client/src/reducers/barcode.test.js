2d9abd99679c9a94d7543e869f1b57363430195a
Arpit Tarang Saxena <arpit.s@greyorange.sg>
2019-01-11 17:20:08 +0530

    fixed [1,0,0] to [1,1,0]; disallowed movement from queue start point to pps

--- client/src/reducers/barcode.test.js
+++ client/src/reducers/barcode.test.js
@@ -91,13 +91,20 @@ describe("ASSIGN-STORABLE", () => {
 });
 
 describe("ADD-QUEUE-BARCODES-TO-PPS", () => {
-  test("should correctly modify multiple queue barcodes with neighbour changes", () => {
+  test("should correctly modify multiple queue barcodes with neighbour changes when first queue coordinate is not next to pps", () => {
+    // TODO: actually queues going straight into pps with turn are wrong and should not be made in the first place
+    // but testing here just to confirm functionality anyway
+    // map and queue:
+    // x pps 4
+    // x  2  5
+    // x  1  x
     var state = makeState(vanilla3x3BarcodeMap);
     var action = {
       type: "ADD-QUEUE-BARCODES-TO-PPS",
       value: {
         pps_id: "1",
-        coordinates: ["1,2", "1,1", "1,0", "0,0", "0,1"]
+        coordinates: ["1,2", "1,1", "1,0", "0,0", "0,1"],
+        pps_coordinate: "1,0"
       }
     };
     var newState = barcodeReducer(state, action);
@@ -125,6 +132,75 @@ describe("ADD-QUEUE-BARCODES-TO-PPS", () => {
       [0, 0, 0],
       [1, 1, 1],
       [1, 1, 1]
+    ]);
+    expect(newState["2,2"].neighbours).toEqual([
+      [1, 1, 1],
+      [1, 1, 1],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]);
+    expect(newState["2,1"].neighbours).toEqual([
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]
+    ]);
+    expect(newState["2,0"].neighbours).toEqual([
+      [0, 0, 0],
+      [1, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]
+    ]);
+    expect(newState["0,2"].neighbours).toEqual([
+      [1, 1, 1],
+      [0, 0, 0],
+      [0, 0, 0],
+      [1, 1, 1]
+    ]);
+  });
+  test("if first queue coordinate is next to pps, don't allow movement to pps", () => {
+    // 3 pps 4
+    // 2  1  x
+    // x  x  x
+    var state = makeState(vanilla3x3BarcodeMap);
+    var action = {
+      type: "ADD-QUEUE-BARCODES-TO-PPS",
+      value: {
+        pps_id: "1",
+        coordinates: ["1,1", "2,1", "2,0", "1,0", "0,0"],
+        pps_coordinate: "1,0"
+      }
+    };
+    var newState = barcodeReducer(state, action);
+    expect(newState["1,1"].neighbours).toEqual([
+      [1, 1, 0],
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1]
+    ]);
+    expect(newState["2,1"].neighbours).toEqual([
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 1, 0],
+      [0, 0, 0]
+    ]);
+    expect(newState["2,0"].neighbours).toEqual([
+      [0, 0, 0],
+      [1, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0]
+    ]);
+    expect(newState["1,0"].neighbours).toEqual([
+      [0, 0, 0],
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 1, 0]
+    ]);
+    expect(newState["0,0"].neighbours).toEqual([
+      [0, 0, 0],
+      [0, 0, 0],
+      [1, 1, 1],
+      [1, 1, 0]
     ]);
   });
 });
