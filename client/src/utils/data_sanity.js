dfcfb56409588582f296080adb73657b3f8663e4
Pushya <pmitra96@gmail.com>
2020-04-17 20:59:51 +0530

    BSS-21704 : revert bad function args in run data sanity
    Reviewers: gaurav.k, chandan.p, tankala.s
    
    Reviewed By: gaurav.k
    
    Subscribers: jenkins
    
    JIRA Issues: BSS-21704
    
    Differential Revision: https://phab.greyorange.com/D7562

--- client/src/utils/data_sanity.js
+++ client/src/utils/data_sanity.js
@@ -4,5 +4,5 @@ import { validateChargersLayout } from "./charger_data_sanity";
 export const runCompleteDataSanity = (normalizedMap) => {
   var barcodesDict = normalizedMap.entities.barcode;
   var chargers = normalizedMap.entities.charger;
-  return validateChargersLayout(barcodesDict, chargers);
+  return validateChargersLayout(chargers, barcodesDict);
 };
