409fdfb2d6cd888d42fc79346fa0262a15430d5a
Akshay Jain <akshay.jain@dotpe.in>
2020-12-24 16:10:50 +0530

    GTOOLS-543: neighbours pattern check

--- client/src/components/Map/Forms/UpdateDirection.js
+++ client/src/components/Map/Forms/UpdateDirection.js
@@ -8,7 +8,8 @@ import { directionSchema } from "utils/forms";
 const neighboursSchema = {
   type: "string",
   title: "Neighbours",
-  default: "1,1,1"
+  default: "1,1,1",
+  pattern: "^[01], *[01], *[01]$"
 };
 
 const schema = {
