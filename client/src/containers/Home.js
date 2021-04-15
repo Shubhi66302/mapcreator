2520f31244a0ec3109941c2b43358881ac7d74d5
Akshay Jain <akshay.jain@dotpe.in>
2021-04-15 15:08:48 +0530

    GTOOLS-586: Pendo
    Summary: GTOOLS-586: Pendo
    
    Reviewers: O6 MHS
    
    Differential Revision: https://phab.greyorange.com/D10051

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,41 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { LinkContainer } from "react-router-bootstrap";
+const pendo = window.pendo;
 
-var Home = () => (
-  <div className="container">
-    <div className="row justify-content-between">
-      <div className="col">
-        <h3 className="display-5">Mapcreator</h3>
+class Home extends Component {
+  componentDidMount() {
+    pendo.initialize({account: {id: "MAPCREATOR-HOME"}});
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col">
+            <h3 className="display-5">Mapcreator</h3>
+          </div>
+          <div className="col pt-auto">
+            {process.env.REACT_APP_VERSION ? (
+              <h4 style={{ textAlign: "right", marginBottom: 0 }}>
+                {process.env.REACT_APP_VERSION}
+              </h4>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+        <div className="list-group">
+          <LinkContainer to="/new">
+            <a className="list-group-item list-group-item-action">Create new map</a>
+          </LinkContainer>
+          <LinkContainer to="/existing">
+            <a className="list-group-item list-group-item-action">
+              Import an existing map
+            </a>
+          </LinkContainer>
+          <LinkContainer to="/version">
+            <a className="list-group-item list-group-item-action">Saved maps</a>
+          </LinkContainer>
+          <LinkContainer to="/guidelines">
+            <a className="list-group-item list-group-item-action">
+              View Guidelines
+            </a>
+          </LinkContainer>
+        </div>
       </div>
-      <div className="col pt-auto">
-        {process.env.REACT_APP_VERSION ? (
-          <h4 style={{ textAlign: "right", marginBottom: 0 }}>
-            {process.env.REACT_APP_VERSION}
-          </h4>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
-    <div className="list-group">
-      <LinkContainer to="/new">
-        <a className="list-group-item list-group-item-action">Create new map</a>
-      </LinkContainer>
-      <LinkContainer to="/existing">
-        <a className="list-group-item list-group-item-action">
-          Import an existing map
-        </a>
-      </LinkContainer>
-      <LinkContainer to="/version">
-        <a className="list-group-item list-group-item-action">Saved maps</a>
-      </LinkContainer>
-      <LinkContainer to="/guidelines">
-        <a className="list-group-item list-group-item-action">
-          View Guidelines
-        </a>
-      </LinkContainer>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Home;
